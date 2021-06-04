e2962af2fdc93f5b6efb1300200c2f0fb03c2613
YatikaThareja <72455816+YatikaThareja@users.noreply.github.com>
2021-06-04 11:41:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+var db;
+var gameState=0;
+var playerCount,form,player,game;
+var allPlayers;
+var cars,car1,car2,car3,car4;
+
+
+function setup(){
+    createCanvas(displayWidth-20,displayHeight-30);
+    db=firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    if (playerCount===4){
+        game.update(1);
+    }
+    if(gameState===1){
+        clear();
+        game.play();
+    }
+}
+
+
